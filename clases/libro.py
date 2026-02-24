b4cbc683826a86256e80ff9866bab34a193609fc
Guillermo <guilleoliva05@gmail.com>
2026-02-24 15:35:59 -0300

    Python console-based Library Management System built using OOP principles. It allows users to register books and users, manage loans and returns, and display information. The system models real-world entities (Book, User, Loan) and maintains data consistency by updating availability status. Features modular design and menu-driven interaction.

---
+++ clases/libro.py
@@ -0,0 +1,20 @@
+class Libro:
+    def __init__(self, titulo, autor, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.disponible = True
+
+    def actualizar_informacion (self, titulo=None,autor=None, isbn=None):
+        if titulo: 
+            self.titulo = titulo
+        if autor:
+            self.autor = autor
+        if isbn:
+            self.isbn = isbn
+
+    def mostrar_informacion(self):
+        estado = "Disponible" if self.disponible else "Prestado"
+        return f"Título: {self.titulo}, Autor: {self.autor}, ISBN: {self.isbn}, Estado: {estado}"
+    
+        
